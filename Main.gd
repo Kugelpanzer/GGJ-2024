2e4356e5ddd4ec1441842c7e38de76152bdb3b0c
Đorđe Mladenović <djolemlad@gmail.com>
2024-01-27 21:39:33 +0100

    hoops work in progress

--- Main.gd
+++ Main.gd
@@ -2,11 +2,19 @@ extends Node
 
 @export var mob_scene: PackedScene
 @export var water_height =540
+@export var hoop_comp = Array()
+@export var x_offset = 100
 var score
+
+var setups : Array = []
+var vector_list_1 = [Vector2(1, 2), Vector2(3, 4), Vector2(5, 6)]
+var vector_list_2 = [Vector2(7, 8), Vector2(9, 10), Vector2(11, 12)]
+
+var current_hoop_list : Array = []
 
 func game_over():
 	$ScoreTimer.stop()
-	$MobTimer.stop()
+	$HoopTimer.stop()
 	$HUD.show_game_over()
 	$Music.stop()
 	$DeathSound.play()
@@ -29,7 +37,6 @@ func _process(delta):
 		$Player.in_water = true
 
 func _on_MobTimer_timeout():
-	"""
 	# Create a new instance of the Mob scene.
 	var mob = mob_scene.instantiate()
 
@@ -38,22 +45,21 @@ func _on_MobTimer_timeout():
 	mob_spawn_location.progress = randi()
 
 	# Set the mob's direction perpendicular to the path direction.
-	var direction = mob_spawn_location.rotation + PI / 2
+	#var direction = mob_spawn_location.rotation + PI / 2
 
 	# Set the mob's position to a random location.
 	mob.position = mob_spawn_location.position
 
 	# Add some randomness to the direction.
-	direction += randf_range(-PI / 4, PI / 4)
-	mob.rotation = direction
+	#direction += randf_range(-PI / 4, PI / 4)
+	#mob.rotation = direction
 
 	# Choose the velocity for the mob.
-	var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
-	mob.linear_velocity = velocity.rotated(direction)
+	#var velocity = Vector2(randf_range(150.0, 250.0), 0.0)
+	#mob.linear_velocity = velocity.rotated(direction)
 
 	# Spawn the mob by adding it to the Main scene.
 	add_child(mob)
-	"""
 
 func _on_ScoreTimer_timeout():
 	score += 1
@@ -61,6 +67,6 @@ func _on_ScoreTimer_timeout():
 
 
 func _on_StartTimer_timeout():
-	$MobTimer.start()
+	$HoopTimer.start()
 	$ScoreTimer.start()
 	
