fdb864455490e3723d2b059acaccb30703fa4166
Đorđe Mladenović <djolemlad@gmail.com>
2024-01-28 15:09:45 +0100

    smilie fixed

--- Smilie.gd
+++ Smilie.gd
@@ -1,5 +1,6 @@
 extends Node2D
 
+var existence_timer = 3
 func _ready():
 	#connect("body_entered", Hoop, "_on_body_entered")
 	pass
@@ -13,5 +14,13 @@ func set_sad():
 	$SmilieSprite.frame = randi_range(0,2)
 # Connect the body_entered signal
 	
-
+func _process(delta):
+	$SmilieSprite.self_modulate.a -= 0.4 *delta
+	position += Vector2.UP *delta *30
+	if existence_timer > 0 :
+		existence_timer -=delta
+	else:
+		queue_free()
+	
+	
 
