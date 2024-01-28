6a54d0bc17b32ed0717d9a7efb6cdc85a6fcdf84
Djordje Mladenovic <djolemlad@gmail.com>
2024-01-28 03:31:18 +0100

    hoop spawn fixed

--- Fail2.gd
+++ Fail2.gd
@@ -2,4 +2,3 @@ extends CollisionShape2D
 
 func _on_fail_2_area_entered(area):
 	get_parent().get_parent().fail2 = true
-	print("fail 2")
--- Success.gd
+++ Success.gd
@@ -3,7 +3,6 @@ extends CollisionShape2D
 
 func _on_success_area_entered(area):
 	get_parent().get_parent().success = true
-	print("success ")
 
 
 func _on_success_area_exited(area):
